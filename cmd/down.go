d1e9b11ce711da75ca523d017403fe823243cf25
Jeffrin <merinjeffrin0@gmail.com>
2026-03-16 12:01:06 +0530

    Update down.go

--- cmd/down.go
+++ cmd/down.go
@@ -1,5 +1,6 @@
 package cmd
 
+import "github.com/spf13/cobra"
 import (
 	"context"
 	"os"
@@ -12,6 +13,10 @@ var downCmd = &cobra.Command{
 	Use:   "down",
 	Short: "Stop all running local cloud services",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		printVerbose("Stopping service orchestration in stub mode")
+		printInfo("Stopping local cloud services...")
+		printWarning("Stub mode: Docker/service shutdown not implemented yet")
+		printSuccess("Status: local services shutdown stub completed")
 		manager, err := docker.NewManager(os.Stdout)
 		if err != nil {
 			return err
